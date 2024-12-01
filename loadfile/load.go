2b0c6c91927f33d9a0a5ffcfe8bf697eb92e836f
Anders Lyager Kaae <anders.kaae@gmail.com>
2024-12-01 10:42:37 +0100

    first commit

---
+++ loadfile/load.go
@@ -0,0 +1,34 @@
+package files
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func loadFile() []string {
+	// Open the file
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("Failed to open the file: %v", err)
+	}
+	defer file.Close()
+
+	// Create a slice to hold lines
+	var lines []string
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// Append each line to the slice
+		lines = append(lines, scanner.Text())
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error while reading the file: %v", err)
+	}
+
+	// Print the resulting list
+	return lines
+}
